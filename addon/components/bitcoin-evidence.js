a91fafac1504c2a5b0f6c93acf17d951aecd70fa
Adrien Montfort <adrien.montfort@gmail.com>
2017-12-29 14:30:20 +0100

    bitcoin-evidence: handle testnet transactions

--- addon/components/bitcoin-evidence.js
+++ addon/components/bitcoin-evidence.js
@@ -2,5 +2,20 @@ import Ember from 'ember';
 import layout from '../templates/components/bitcoin-evidence';
 
 export default Ember.Component.extend({
-  layout
+  layout,
+
+  didReceiveAttrs() {
+    const e = this.get('evidence');
+    const txid = e.proof.txid;
+    if (e.provider.match(/test/)) {
+      this.set('url', `https://live.blockcypher.com/btc-testnet/tx/${txid}`);
+    } else {
+      this.set('url', `https://blockchain.info/tx/${txid}`);
+    }
+
+    this.set(
+      'merklePathPresent',
+      this.get('evidence').proof.batch.merklePath.length > 0
+    );
+  }
 });
