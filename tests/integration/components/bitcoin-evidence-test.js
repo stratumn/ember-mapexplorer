2e5add67708dc643d631bbb4b64c32d428e97cb8
Adrien Montfort <adrien.montfort@gmail.com>
2017-12-29 14:30:20 +0100

    bitcoin-evidence: handle testnet transactions

--- tests/integration/components/bitcoin-evidence-test.js
+++ tests/integration/components/bitcoin-evidence-test.js
@@ -12,10 +12,21 @@ moduleForComponent(
 test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('evidence', {
+    backend: 'bcbatch',
+    provider: 'bitcoin:test3',
+    proof: {
+      batch: {
+        timestamp: 1507798943,
+        merkleRoot:
+          '9449ecec772bf5b37ef9d204839d3a6a7b7a22d6c75354e193340c3eec0ee289',
+        merklePath: []
+      },
+      txid: '3f94064ebfee38b763493ef5c073baf4320933555a921741bcb26c77d9b26924'
+    }
+  });
 
-  this.render(
-    hbs`{{bitcoin-evidence evidence='{state: "COMPLETE",backend: "bcbatch",provider: "dummytimestamper",proof: {batch: {timestamp: 1507798943,merkleRoot: "9449ecec772bf5b37ef9d204839d3a6a7b7a22d6c75354e193340c3eec0ee289",merklePath: [ ]},txid: "3f94064ebfee38b763493ef5c073baf4320933555a921741bcb26c77d9b26924"}}'}}`
-  );
+  this.render(hbs`{{bitcoin-evidence evidence=evidence}}`);
 
   assert.equal(0, 0);
 });
